b2607ff492e5e4793e3035561d0fd8a71121029e
Mathis Carvalho <carvalh49u@etu.univ-lorraine.fr>
2025-01-07 14:31:06 +0100

    Mise à jour de Grille et TestGrille

--- Grille.py
+++ Grille.py
@@ -29,40 +29,60 @@ class Grille():
     
     def getNbColonnes(self):
         return self.__nb_colonnes
-
+    
+    def setNbLignes(self, nb_lignes : int):
+        if ((nb_lignes < 2 and self.__nb_colonnes == 1) or (self.__nb_colonnes < 2 and nb_lignes == 1)):
+            raise ValueError('Votre grille ne respecte pas les normes')
+        else:
+            self.__nb_lignes = nb_lignes
+        
+    def setNbColonnes(self, nb_colonnes : int):
+        if ((self.__nb_lignes < 2 and nb_colonnes == 1) or (nb_colonnes < 2 and self.__nb_lignes == 1)):
+            self.__nb_colonnes = nb_colonnes
+        else:
+            raise ValueError('Votre grille ne respecte pas les normes')
 
     def __init__(self, nombre_lignes, nombre_colonnes):
         self.__nb_lignes = nombre_lignes
         self.__nb_colonnes = nombre_colonnes
 
-        self.grille = []
-
-        self.creation_grille_de_jeu()
+        self.plateau = []
 
 
     # Créer une grille de jeu:
-    def creation_grille_de_jeu(self):
-        self.grille = []
-        if self.getNbColonnes() >= 10 and self.getNbLignes() >= 10:
-            for i in range(self.getNbLignes()):
-                self.grille.append([])
-                for j in range(self.getNbColonnes()):
-                    self.grille[i].append("-")
-            return True
-        else:
-            raise ValueError('Votre grille doit avoir au moins 10 lignes et 10 colonnes')
+    def creation_grille(self):
+        try:
+            self.setNbLignes(self.__nb_lignes)
+            self.setNbColonnes(self.__nb_colonnes)
+        except:
+            raise ValueError('Votre grille ne respecte pas les normes')
             return False
+
+        self.plateau = []
+        #if self.getNbColonnes() >= 10 and self.getNbLignes() >= 10:
+        for i in range(self.getNbLignes()):
+            self.plateau.append([])
+            for j in range(self.getNbColonnes()):
+                self.plateau[i].append("-")
+        return True
+        #else:
+            #raise ValueError('Votre grille doit avoir au moins 10 lignes et 10 colonnes')
+            #return False
 
 
 
 # Fonction d'affichage
-def afficher_grille(grille) :
-    for ligne in grille:
+def afficher_grille(plateau) :
+    result = ""
+    for ligne in plateau:
+        result += " ".join(ligne) + "\n"
         print(" ".join(ligne))
-    print("")
+    return result
 
 def afficher_couple_grilles(grille1, grille2):
+    result = "     Vos navires :                      Champ de tir :\n"
     print("     Vos navires :                      Champ de tir :")
     for index_ligne in range(len(grille1))  :
+        result += "     "+" ".join(grille1[index_ligne])+"                "+" ".join(grille2[index_ligne]) + "\n"
         print("     "+" ".join(grille1[index_ligne])+"                "+" ".join(grille2[index_ligne]))
-    print("")
+    return result
--- TestGrille.py
+++ TestGrille.py
@@ -1,21 +1,17 @@
 import unittest
 from Grille import Grille, afficher_grille, afficher_couple_grilles
 
-class MyTestGrilleCase(unittest.TestCase):
-    
-    def setUp(self) -> None:
-        self.grille1: Grille = Grille(10,10)
-        self.grille2: Grille = Grille(0,0)
-        self.grille3: Grille = Grille(-1,-1)
-        self.grille4: Grille = Grille(10,10)
+class TestGrille(unittest.TestCase):
     
     def test_init_grille(self):
-    
-        self.assertEqual(10, self.grille1.get_nb_lignes())
-        self.assertEqual(10, self.grille1.get_nb_colonne())
+        grille = Grille(2, 2)
+        self.assertEqual(2, grille.getNbLignes())
+        self.assertEqual(2, grille.getNbColonnes())
         
-    def test_creation_grille_de_jeu_input_positives(self):
-        self.assertEqual(
+    def test_creation_grille_nominal(self):
+        grille = Grille(10,10)
+        self.assertTrue(grille.creation_grille())
+        self.assertEqual(grille.plateau,
                 [['-','-','-','-','-','-','-','-','-','-'],
                 ['-','-','-','-','-','-','-','-','-','-'],
                 ['-','-','-','-','-','-','-','-','-','-'],
@@ -25,31 +21,29 @@ class MyTestGrilleCase(unittest.TestCase):
                 ['-','-','-','-','-','-','-','-','-','-'],
                 ['-','-','-','-','-','-','-','-','-','-'],
                 ['-','-','-','-','-','-','-','-','-','-'],
-                ['-','-','-','-','-','-','-','-','-','-']], self.grille1.grille)
+                ['-','-','-','-','-','-','-','-','-','-']])
 
-
-    def test_creation_grille_de_jeu_valide(self):
-        if self.assertEqual([], self.grille1.grille):
-            "Grille valide"
-        else:
-            "Grille invalide"
-        if self.assertEqual([], self.grille2.grille):
-            "Grille valide"
-        else:
-            "Grille invalide"
-
-    def test_creation_grille_de_jeu_invalide(self):
-        if not self.assertEqual([], self.grille3.grille):
-            "Grille invalide"
-        else:
-            "Grille valide"
-        if not self.assertEqual([], self.grille4.grille):
-            "Grille invalide"
-        else:
-            "Grille valide"
-
+    def test_creation_grille_impossible_technique(self):
+        
+        grille = Grille(-1, -1)
+        self.assertFalse(grille.creation_grille())
+        #self.assertEqual(self.grille1.plateau,
+            #[['-','-'],
+             #['-','-']])
+             
+    def test_creation_grille_impossible_fonctionnel(self):
+        grille = Grille(0, 0)
+        self.assertFalse(grille.creation_grille())
+        grille = Grille(1, 1)
+        self.assertFalse(grille.creation_grille())
+        grille = Grille(1, 2)
+        self.assertTrue(grille.creation_grille())
+        grille = Grille(2, 1)
+        self.assertTrue(grille.creation_grille())
 
     def test_afficher_grille(self):
+        grille = Grille(10,10)
+        self.assertTrue(grille.creation_grille())
         self.assertEqual(("- - - - - - - - - -\n"
                           "- - - - - - - - - -\n"
                           "- - - - - - - - - -\n"
@@ -59,11 +53,15 @@ class MyTestGrilleCase(unittest.TestCase):
                           "- - - - - - - - - -\n"
                           "- - - - - - - - - -\n"
                           "- - - - - - - - - -\n"
-                          "- - - - - - - - - -\n"), afficher_grille(self.grille1.grille))
+                          "- - - - - - - - - -\n"), afficher_grille(grille.plateau))
 
 
     def test_afficher_couple_grilles(self):
-        self.assertEqual((
+        grille1 = Grille(10,10)
+        grille2 = Grille(10,10)
+        self.assertTrue(grille1.creation_grille())
+        self.assertTrue(grille2.creation_grille())
+        self.assertEqual((afficher_couple_grilles(grille1.plateau, grille2.plateau),
             "     Vos navires :                      Champ de tir :\n"
             "     - - - - - - - - - -                - - - - - - - - - -\n"
             "     - - - - - - - - - -                - - - - - - - - - -\n"
@@ -74,7 +72,7 @@ class MyTestGrilleCase(unittest.TestCase):
             "     - - - - - - - - - -                - - - - - - - - - -\n"
             "     - - - - - - - - - -                - - - - - - - - - -\n"
             "     - - - - - - - - - -                - - - - - - - - - -\n"
-            "     - - - - - - - - - -                - - - - - - - - - -\n"), afficher_couple_grilles(self.grille1.grille, self.grille4.grille))
+            "     - - - - - - - - - -                - - - - - - - - - -\n"))
     
-    if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__': 
+    unittest.main()
