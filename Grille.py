ab97eef75fe83d4cf79b9de64d528816542e2121
Remy Courte <courte12u@etu.univ-lorraine.fr>
2025-01-07 14:42:57 +0100

    mise à jour grille et des tests

--- Grille.py
+++ Grille.py
@@ -18,35 +18,68 @@ pour la classe BatailleNavale() et pour la classe ChoixStrategie().
 
 
 class Grille():
+    # Variables privées
+    __nb_lignes = 0
+    __nb_colonnes = 0
 
+    # Constructeur
     def __init__(self, nombre_lignes, nombre_colonnes):
-        self.nb_lignes = nombre_lignes
-        self.nb_colonnes = nombre_colonnes
-
+        self.__nb_lignes = nombre_lignes
+        self.__nb_colonnes = nombre_colonnes
         self.grille = []
+        
+        
+    # Getters
+    def get_nb_lignes(self):
+        return self.__nb_lignes
 
-        self.creation_grille_de_jeu()
+    def get_nb_colonne(self):
+        return self.__nb_colonnes
 
 
-    # Créer une grille de jeu:
+    # Setters
+    def set_nb_lignes(self, nb_lignes:int):
+        if nb_lignes >= 2 :
+            self.__nb_lignes = nb_lignes
+        else :
+            raise ValueError("Le nombre de lignes minimum est 2 !")
+
+    def set_nb_colonnes(self, nb_colonnes:int):
+        if nb_colonnes >=2 :
+            self.__nb_colonnes = nb_colonnes
+        else :
+            raise ValueError("Le nombre minimum de colonne est 2 !")
+
+
     def creation_grille_de_jeu(self):
-        self.grille = []
-        for i in range(self.nb_lignes):
-            self.grille.append([])
-            for j in range(self.nb_colonnes):
-                self.grille[i].append("-")
+        try: 
+            self.set_nb_lignes(self.__nb_lignes)
+            self.set_nb_colonnes(self.__nb_colonnes)
+        except ValueError:
+            raise ValueError("Les valeurs saisies sont trop petites, veuillez saisir de nouvelles valeurs.")
+        
+        self.grille = []  
+        for i in range(self.__nb_lignes):
+            ligne = []
+            for j in range(self.__nb_colonnes):
+                ligne.append("-")
+            self.grille.append(ligne) 
         return True
 
 
 
 # Fonction d'affichage
 def afficher_grille(grille) :
+    result = ""
     for ligne in grille:
+        result += " ".join(ligne)+"\n"
         print(" ".join(ligne))
-    print("")
+    return result
 
 def afficher_couple_grilles(grille1, grille2):
-    print("     Vos navires :                      Champ de tir :")
+    result = ""
+    result += "     Vos navires :                      Champ de tir :\n"
     for index_ligne in range(len(grille1))  :
-        print("     "+" ".join(grille1[index_ligne])+"                "+" ".join(grille2[index_ligne]))
-    print("")
+        result += "     "+" ".join(grille1[index_ligne])+"                "+" ".join(grille2[index_ligne])+"\n"
+    print(result)
+    return(result)
--- TestGrille.py
+++ TestGrille.py
@@ -1,7 +1,73 @@
-# -*- coding: utf-8 -*-
+import unittest
+from Grille import Grille, afficher_grille, afficher_couple_grilles
 """
-Created on Tue Dec 17 09:27:41 2024
-
-@author: courte12u
+utilisateur doit pouvoir choisir la taille de la grille
 """
 
+class TestGrille(unittest.TestCase):
+
+    def test_creation_grille_valide(self) -> None:
+        grille_valide = Grille(2,2)  
+        self.assertTrue(grille_valide.creation_grille_de_jeu())
+    
+    def test_creation_grille_invalide_fonctionnelle(self)-> None :
+            grille_invalide = Grille(1, 1)  
+            with self.assertRaises(ValueError):
+                grille_invalide.creation_grille_de_jeu()
+                
+    def test_creation_grille_invalide_technique(self)-> None :
+            grille_invalide = Grille(-1, -1)  
+            with self.assertRaises(ValueError):
+                grille_invalide.creation_grille_de_jeu()
+      
+    def test_affichage_grille_simple(self)-> None :
+
+        self.grille = [
+            ["-", "-"],
+            ["-", "-"]
+        ]
+
+        resultat_grille_affiche = afficher_grille(self.grille)
+
+
+        resultat_grille_affichage_esperer = "- -\n- -\n"
+
+
+        self.assertEqual(resultat_grille_affiche, resultat_grille_affichage_esperer)
+        
+    def test_affichage_grille_double(self) -> None:
+        # Initialisation des grilles
+        grille_1 = Grille(10, 10)
+        grille_2 = Grille(10, 10)
+        
+        # Affichage des grilles via la fonction
+        resultat = afficher_couple_grilles(grille_1.grille, grille_2.grille)
+    
+        # Résultat attendu
+        resultat_attendu = """     Vos navires :                      Champ de tir :
+         - - - - - - - - - -                - - - - - - - - - -
+         - - - - - - - - - -                - - - - - - - - - -
+         - - - - - - - - - -                - - - - - - - - - -
+         - - - - - - - - - -                - - - - - - - - - -
+         - - - - - - - - - -                - - - - - - - - - -
+         - - - - - - - - - -                - - - - - - - - - -
+         - - - - - - - - - -                - - - - - - - - - -
+         - - - - - - - - - -                - - - - - - - - - -
+         - - - - - - - - - -                - - - - - - - - - -
+         - - - - - - - - - -                - - - - - - - - - -"""
+
+        # Comparaison des résultats
+        self.assertEqual(resultat, resultat_attendu)
+
+        
+                             
+if __name__ == '__main__': 
+    unittest.main()             
+               
+               
+               
+               
+               
+               
+               
+               
