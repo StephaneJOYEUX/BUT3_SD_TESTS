47d6c7362d7388de87fcdf5f21280cb100cfe7b9
Thibault VIVIER <thibault.vivier0@gmail.com>
2025-01-09 18:27:16 +0100

    commencement des modification sur les classes Strategie, ChoixStrategie, CreationStrategie, pour pouvoir remplacer le stockage des navire, des input_strategie avec un set et un pd.Dataframe : creation de classe Strategie_2 et equivalent pour eviter de supprimer tout le code existant

--- ChoixStrategie.py
+++ ChoixStrategie.py
@@ -47,6 +47,8 @@ Le référentiel :
 La classe contient aussi des méthodes de lecture et d'écriture dans un fichier de sauvegarde annexe (.txt).
 """
 from ast import literal_eval
+
+import pandas as pd
 from unidecode import unidecode
 from CreationStrategie import CreationStrategie
 from Grille import Grille, afficher_grille
@@ -266,3 +268,211 @@ class ChoixStrategie() :
     def get_strategie(self):
         return self.strategie
 
+
+
+
+
+
+
+
+
+class ChoixStrategie_2() :
+    def __init__(self, pseudo_joueur, navires, Grille = Grille(10,10), test : bool = False):
+
+        self.navires = navires
+        self.referentiel : set[Strategie]
+        self.lire_fichier_sauvegarde()
+
+        self.instance_grille=Grille
+        self.pseudo_joueur = pseudo_joueur
+
+        self.strategie : Strategie
+
+        if not test :
+            # lancement de la méthode principale.
+            self.main()
+
+        # dernière méthode : écriture des données du référentiel dans le fichier de sauvegarde.
+        self.ecrire_fichier_sauvegarde()
+
+    # Lecture du fichier et récupération des strategie enregistrées dans le référentiel.
+    def lire_fichier_sauvegarde(self):
+        # Ouverture du fichier + lecture des lignes une à une + fermeture du fichier.
+        donnees_lecture = pd.read_csv('sauvegardes_strategies_2.csv', encoding="UTF-8")
+
+        for line in donnees_lecture.value :
+            #self.referentiel.add(Strategie(line))
+            pass
+
+
+
+
+    # On écrit le fichier de sauvegarde à partir des données du référentiel.
+    def ecrire_fichier_sauvegarde(self):
+        # Variable locale pour 'protéger' notre référentiel.
+        liste_ecriture = []
+
+
+        # Formatage des données pour l'écriture (passage en str)
+
+        for instance_strategie in self.referentiel :
+            donnee = instance_strategie.informations
+            # Vérification du type (attendu : dict)
+            donnee = str(donnee) + '\n'
+            liste_ecriture.append(donnee)
+
+
+        # Création d'une chaine unique -> application unique de la fonction write()
+        chaine_ecriture = "".join(liste_ecriture)
+
+        # Ouverture du fichier + écriture + fermeture du fichier
+        test = open('sauvegardes_strategies_2.csv', 'w', encoding='UTF-8')
+        test.write(chaine_ecriture)
+        test.close()
+
+
+    # Méthode principale qui appelle toutes les autres en fonction des choix du joueur.
+    def main(self):
+
+        print(f"C'est à {self.pseudo_joueur} de choisir sa stratégie de bataille.")
+        choix_choisir_creer = f"{self.pseudo_joueur}, voulez-vous choisir une stratégie enregistrée ou en créer une nouvelle ?"
+        choix_choisir = 'choisir'
+        choix_creer = 'creer'
+        # validation de l'input avec la méthode associée
+        choix = self.valider_input_utilisateur(choix_choisir_creer, choix_choisir, choix_creer)
+
+        # Définition des variables associée aux choix OUI/NON.
+        # Utile pour la méthode de validation d'input.
+        choix_oui = 'o'
+        choix_non = 'n'
+
+        if choix == 'creer' :
+            # Appel de la classe CreationStrategie() -> lancement du code de création
+            # Appel de la methode get_instance_strategie() qui retourne l'instance de la classe Strategie() créée.
+            self.strategie = CreationStrategie(self.navires, self.instance_grille).get_instance_strategie()
+
+            # Création de la chaine rentrée ensuite en paramètre de la fonction input()
+            choix_action_enregistrement = "Voulez-vous enregistrer la stratégie créée ?"
+            # validation de l'input avec la méthode associée
+            choix_enregistrement = self.valider_input_utilisateur(choix_action_enregistrement, choix_oui, choix_non)
+
+            if choix_enregistrement == 'o' :
+                # Ajout de la stratégie au référentiel.
+                # il s'agit ici d'une instance de la classe Strategie().
+                self.referentiel.append(self.strategie)
+                print("La stratégie a bien été enregistrée.")
+            else :
+                print('Vous avez choisi de ne pas enregistrer la strategie.')
+
+
+            # Affichage de la strategie du joueur + Fin du main
+            print("")
+            print(f"{self.pseudo_joueur}, votre stratégie a bien été définie.")
+            print("")
+
+            self.instance_grille.create()
+            self.strategie.placement_navires_joueur(self.instance_grille.plateau, self.strategie.informations)
+            afficher_grille(self.instance_grille.plateau)
+            return True
+
+        elif choix == 'choisir':
+
+            strategie_correctement_choisie = False
+            while not strategie_correctement_choisie :
+                self.instance_grille.create()
+                print('Voici la liste des stratégies enregistrée :')
+
+                for index_strat in range(len(self.referentiel)) :
+                    print("Stratégie n°"+str(index_strat)+ "  "+str(self.referentiel[index_strat].informations))
+                print("")
+
+
+                valider_choix_numero_strategie = False
+
+                while not valider_choix_numero_strategie :
+                    choix_numero_strategie = input("Quelle stratégie voulez-vous choisir ?  (Choisir une numéro de stratégie)\n")
+                    print("")
+
+                    try :
+                        choix_numero_strategie = int(choix_numero_strategie)
+
+                        # On associe la strategie choisie en input à la variable locale strategie_choisie.
+                        strategie_choisie = self.referentiel[choix_numero_strategie]
+                        print(strategie_choisie.informations)
+
+                        valider_choix_numero_strategie = True
+                    except IndexError :
+                        print(f"La stratégie n°{choix_numero_strategie} n'existe pas !")
+                        print("Choisissez une stratégie existante.")
+                    except :
+                        print("Erreur de saisie !")
+                        print('Choisissez le numéro de la stratégie parmis les stratégie existantes.')
+
+
+
+                print(f"Vous avez choisi la stratégie n°{choix_numero_strategie} :\n")
+
+                # On place la strategie dans la grille
+                strategie_choisie.placement_navires_joueur(self.instance_grille.plateau, strategie_choisie.informations)
+                # On l'affiche
+                afficher_grille(self.instance_grille.plateau)
+
+                # Confirmation du choix de la strategie
+                choix_action_confirmation_strategie = "Confirmez votre choix ?"
+                # validation de l'input
+                confirmation_choix_strategie = self.valider_input_utilisateur(choix_action_confirmation_strategie, choix_oui, choix_non)
+
+                # Condition de sortie de boucle -> OK
+                if confirmation_choix_strategie == 'o' :
+                    print(f'Vous avez bien choisi la stratégie n°{choix_numero_strategie}')
+                    self.strategie = strategie_choisie
+                    strategie_correctement_choisie = True
+
+
+            # Affichage de la strategie du joueur + Fin du main
+            print("")
+            print(f"{self.pseudo_joueur}, votre stratégie a bien été définie.")
+            print("")
+
+            self.instance_grille.create()
+            self.strategie.placement_navires_joueur(self.instance_grille.plateau, self.strategie.informations)
+            afficher_grille(self.instance_grille.plateau)
+            return True
+
+
+    # Méthode permettant de protéger les inputs utilisateur et de gérer les cas d'erreur.
+    def valider_input_utilisateur(self, requette : str, choix_1 : str, choix_2 : str):
+        # condition de validité de l'input
+        validite_choix_utilisateur = False
+
+        # boucle de vérification / modification de la valeur précédente.
+        while not validite_choix_utilisateur :
+            choix_utilisateur = input(requette + '  (' + choix_1 + '/' + choix_2 + ')\n')
+
+            try :
+                # Suppression des accents potentiels
+                choix_utilisateur = unidecode(choix_utilisateur)
+                # Passage de la chaine en minuscule
+                choix_utilisateur = choix_utilisateur.lower()
+                choix_1 = choix_1.lower()
+                choix_2 = choix_2.lower()
+
+                assert choix_utilisateur == choix_1 or choix_utilisateur == choix_2
+                validite_choix_utilisateur = True
+
+            except AssertionError:
+                print("Erreur de saisie !")
+                print("Vous devez choisir l'une des propositions\n")
+            except :
+                print('Erreur !')
+                print('Suivez les instructions pour pouvoir jouer correctement !\n')
+
+        print("")
+        return choix_utilisateur
+
+
+    def get_referentiel(self):
+        return self.referentiel
+
+    def get_strategie(self):
+        return self.strategie
--- TestStrategie.py
+++ TestStrategie.py
@@ -1,10 +1,17 @@
 import unittest
-from Strategie import Strategie
+from Strategie import Strategie, Strategie_2
+from Navire import Navire
 
 
 class TestStrategie(unittest.TestCase) :
     def setUp(self) -> None:
-        pass
+        self.cuirasse = Navire(nom="cuirassé", taille=4)
+        self.fregate = Navire(nom="frégate", taille=3)
+        self.sous_marin = Navire(nom="sous-marin", taille=3)
+        self.torpilleur = Navire(nom="torpilleur", taille=2)
+        self.porte_avions = Navire(nom="porte-avions", taille=5)
+
+        self.navires = {self.cuirasse, self.fregate, self.sous_marin, self.torpilleur, self.porte_avions}
 
     def test_set_navires_cas_nominal(self)->None:
         navires = {'Torpilleur': [2, 'T'], 'Sous-marin': [3, 'S'], 'Frégate': [3, 'F'], 'Cuirassé': [4, 'C'],
@@ -48,3 +55,35 @@ class TestStrategie(unittest.TestCase) :
                                   'Frégate': [3, 3, 5, 'E'],
                                   'Cuirassé': [4, 5, 6, 'O'],
                                   'Porte-avions': [5, 9, 9, 'N']}, self.strategie.get_informations())
+
+
+
+class TestStrategie_2(unittest.TestCase) :
+    def setUp(self) -> None:
+        self.cuirasse = Navire(nom="cuirassé", taille=4)
+        self.fregate = Navire(nom="frégate", taille=3)
+        self.sous_marin = Navire(nom="sous-marin", taille=3)
+        self.torpilleur = Navire(nom="torpilleur", taille=2)
+        self.porte_avions = Navire(nom="porte-avions", taille=5)
+
+
+    def test_set_navires_cas_nominal(self)->None:
+        navires = {self.cuirasse, self.fregate, self.sous_marin, self.torpilleur, self.porte_avions}
+        inputs_strategie = {'Torpilleur': [2, 1, 1, 'S'],
+                                  'Sous-marin': [3, 5, 1, 'S'],
+                                  'Frégate': [3, 3, 5, 'E'],
+                                  'Cuirassé': [4, 5, 6, 'O'],
+                                  'Porte-avions': [5, 9, 9, 'N']}
+        self.strategie = Strategie_2(inputs_strategie=inputs_strategie, navires=navires)
+
+        self.strategie.set_navires(navires=self.strategie.navires)
+        self.strategie.set_informations(self.strategie.informations)
+
+        self.assertEqual(navires, self.strategie.get_navires())
+        self.assertEqual({'Torpilleur': [2, 1, 1, 'S'],
+                                  'Sous-marin': [3, 5, 1, 'S'],
+                                  'Frégate': [3, 3, 5, 'E'],
+                                  'Cuirassé': [4, 5, 6, 'O'],
+                                  'Porte-avions': [5, 9, 9, 'N']}, self.strategie.get_informations())
+
+
