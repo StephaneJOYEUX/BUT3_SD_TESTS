42daa7952f52338ef6653f8879f08eaea224d961
Vincent Moulin <moulin54u@etu.univ-lorraine.fr>
2025-01-20 10:46:21 +0100

    fix_strategie

--- TestStrategie.py
+++ TestStrategie.py
@@ -17,7 +17,7 @@ class TestStrategie(unittest.TestCase):
             'torpilleur': [2, 'D']
         }
 
-    def test_verifier_validite_placement_valide(self):
+    def test_verifier_validite_valide(self):
         strategie_valide = {
             'porte_avion': [5, 1, 1, 'E'],
             'croiseur': [4, 3, 1, 'S'],
@@ -28,7 +28,7 @@ class TestStrategie(unittest.TestCase):
         strategie = Strategie(strategie_valide, self.navires, self.grille)
         self.assertTrue(strategie.verifier_validite())
 
-    def test_verifier_validite_placement_chevauchement(self):
+    def test_verifier_validite_chevauchement(self):
         strategie_chevauchement = {
             'porte_avion': [5, 1, 1, 'E'],
             'croiseur': [4, 1, 3, 'S']
@@ -42,17 +42,17 @@ class TestStrategie(unittest.TestCase):
             'croiseur': [4, 3, 1, 'S']
         }
         strategie = Strategie(strategie_valide, self.navires, self.grille)
-        self.assertTrue(strategie.placement_navires_joueur(self.grille.grille, strategie_valide))
+        self.assertTrue(strategie.placement_navires_joueur(self.grille.plateau, strategie_valide))
 
     def test_placement_un_navire_valide(self):
         strategie = {'porte_avion': [5, 1, 1, 'E']}
         strategie_instance = Strategie(strategie, self.navires, self.grille)
-        self.assertTrue(strategie_instance.placement_un_navire(self.grille.grille, strategie, 'porte_avion'))
+        self.assertTrue(strategie_instance.placement_un_navire(self.grille.plateau, strategie, 'porte_avion'))
 
     def test_placement_un_navire_hors_grille(self):
         strategie = {'porte_avion': [5, 1, 8, 'E']}
         strategie_instance = Strategie(strategie, self.navires, self.grille)
-        self.assertFalse(strategie_instance.placement_un_navire(self.grille.grille, strategie, 'porte_avion'))
+        self.assertFalse(strategie_instance.placement_un_navire(self.grille.plateau, strategie, 'porte_avion'))
 
     def test_placement_un_navire_chevauchement(self):
         strategie = {
@@ -60,8 +60,8 @@ class TestStrategie(unittest.TestCase):
             'croiseur': [4, 1, 3, 'S']
         }
         strategie_instance = Strategie(strategie, self.navires, self.grille)
-        strategie_instance.placement_un_navire(self.grille.grille, strategie, 'porte_avion')
-        self.assertFalse(strategie_instance.placement_un_navire(self.grille.grille, strategie, 'croiseur'))
+        strategie_instance.placement_un_navire(self.grille.plateau, strategie, 'porte_avion')
+        self.assertFalse(strategie_instance.placement_un_navire(self.grille.plateau, strategie, 'croiseur'))
 
     def test_affichage_strategie_valide(self):
         strategie_valide = {
@@ -70,6 +70,7 @@ class TestStrategie(unittest.TestCase):
         strategie_instance = Strategie(strategie_valide, self.navires, self.grille)
         self.assertTrue(strategie_instance.verifier_validite())
         strategie_instance.affichage_strategie()
+
 
 if __name__ == '__main__':
     unittest.main()
